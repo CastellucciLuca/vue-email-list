2df6ac4862e9d17eccf49812fd11080bc1e6fb3a
CastellucciLuca <castellucciluca98@gmail.com>
2022-12-21 15:35:37 +0100

    email-list+10 randoms email

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const {createApp} = Vue;
+
+createApp({
+	data() {
+		return{
+        emailList : [],
+		}
+	},
+
+    methods: {
+
+        generateRandomEmail(){
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const result = response.data.response;
+                    console.log(result);
+                    this.emailList.push(result);
+                });
+                
+            }
+        },
+    },   
+    created(){
+        this.generateRandomEmail();
+    }
+}).mount ('#app')
